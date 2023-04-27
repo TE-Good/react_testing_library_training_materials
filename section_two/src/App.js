2ef4d6cc2d5b8c2fb6186863768249c8192c8266
Tom Good <tom.good@eigentech.com>
2023-04-27 13:47:54 +0100

    Created todo list

--- section_two/src/App.js
+++ section_two/src/App.js
@@ -7,31 +7,39 @@ import "./App.css";
 
 const PRIORITIES = ["Low", "Medium", "High"];
 
+
 const Form = ({ onClick }) => {
-  const [task, setTask] = useState();
+  const [task, setTask] = useState("");
   const [priority, setPriority] = useState(PRIORITIES[0]);
-  const [description, setDescription] = useState();
+  const [description, setDescription] = useState("");
+  const [alreadyComplete, setAlreadyComplete] = useState(false);
+
+  const clearForm = () => {
+    setTask("");
+    setPriority(PRIORITIES[0]);
+    setDescription("");
+  };
 
   return (
     <form>
       <div className="row">
         <div className="six columns">
-          <label htmlFor="exampleEmailInput">Task</label>
+          <label htmlFor="taskInput">Task</label>
           <input
             className="u-full-width"
             type="text"
             placeholder="Task name.."
-            id="exampleEmailInput"
+            value={task}
             onChange={(e) => setTask(e.target.value)}
           />
         </div>
 
         <div className="six columns">
-          <label htmlFor="exampleRecipientInput">Priority</label>
+          <label htmlFor="priorityInput">Priority</label>
           <select
             className="u-full-width"
-            id="exampleRecipientInput"
             onChange={(e) => setPriority(e.target.value)}
+            value={priority}
           >
             {PRIORITIES.map((priority) => (
               <option value={priority} key={priority}>
@@ -42,87 +50,83 @@ const Form = ({ onClick }) => {
         </div>
       </div>
 
-      <label htmlFor="exampleMessage">Description</label>
+      <label htmlFor="descriptionMessage">Description</label>
       <textarea
         className="u-full-width"
         placeholder="More information.."
-        id="exampleMessage"
         value={description}
-        onChange={e => setDescription(e.target.value)}
+        onChange={(e) => setDescription(e.target.value)}
       ></textarea>
 
       <label className="example-send-yourself-copy">
-        <input type="checkbox" />
+        <input
+          type="checkbox"
+          onClick={() => setAlreadyComplete(!alreadyComplete)}
+        />
         <span className="label-body">Already complete</span>
       </label>
       <input
         className="button-primary"
         type="submit"
+        disabled={!task}
         onClick={(e) => {
-          e.preventDefault()
-          onClick({task, priority, description})
+          e.preventDefault();
+          onClick({ task, priority, description, alreadyComplete });
+          clearForm();
         }}
       />
     </form>
   );
 };
 
-const Table = () => (
+const TableRow = ({ task, priority, description, alreadyComplete }) => (
+  <tr>
+    <td>{task}</td>
+    <td>{priority}</td>
+    <td>{description}</td>
+    <td>
+      <input type="checkbox" defaultChecked={alreadyComplete} />
+    </td>
+  </tr>
+);
+
+const Table = ({children}) => (
   <table className="u-full-width">
     <thead>
       <tr>
         <th>Task</th>
         <th>Priority</th>
         <th>Description</th>
+        <th>Complete</th>
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <td>Dave Gamache</td>
-        <td>26</td>
-        <td>Male</td>
-      </tr>
-      <tr>
-        <td>Dwayne Johnson</td>
-        <td>42</td>
-        <td>Male</td>
-      </tr>
+      {children}
     </tbody>
   </table>
 );
 
 function App() {
-  const onSubmitForm = (values) => {
-    console.log(values);
+  const [taskList, setTaskList] = useState([]);
+
+  const onSubmitForm = (newTask) => {
+    const updatedTaskList = [...taskList, newTask];
+    setTaskList(updatedTaskList);
   };
 
   return (
     <div className="container">
+      <br />
       <h1>ToDo List</h1>
+      <br />
+
       <Form onClick={onSubmitForm} />
 
-      <Table />
+      <Table>
+        {taskList.map((props, i) => <TableRow key={i} {...props} />)}
+      </Table>
     </div>
   );
 }
 
 export default App;
-
-const oldApp = (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
-);
